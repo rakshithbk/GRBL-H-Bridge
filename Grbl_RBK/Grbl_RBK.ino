bbf1b617f156da3a762eed56ed0c185939a21781
Rakshith B K <rakkurbk@gmail.com>
2018-01-10 16:26:54 +0530

    Add files via upload

---
+++ Grbl_RBK/Grbl_RBK.ino
@@ -0,0 +1,59 @@
+
+/*  ~ GRBL with L293d (H-Bridge) and DC Motor Pen mechanism ~
+              ╦═╗╔╗ ╦╔═
+      By--    ╠╦╝╠╩╗╠╩╗     - 2016  Rakshith BK
+              ╩╚═╚═╝╩ ╩         
+ *  
+ *  No licences no copyrights, well may be MIT.
+ *  Use modify distribute and do whatever you want
+ *  And even though this code will not launch any Nukes, 
+ *  I'm not responsible for any destruction caused by it.!
+ *  
+ *  X-axis stepper --> 8, 9, 10, 11
+ *  Y-axis stepper --> 4, 5, 6, 7
+ *  DC motor pin for Pen --> 12     for Pen Up - M4
+ *                                  for Pen Down - M3
+ *  
+ *  
+ *  For Details and working -- 
+ */
+
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <util/delay.h>
+#include <math.h>
+#include <inttypes.h>    
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+
+#include "config.h"
+#include "nuts_bolts.h"
+#include "settings.h"
+#include "eeprom.h"
+#include "gcode.h"
+#include "limits.h"
+#include "motion_control.h"
+#include "planner.h"
+#include "print.h"
+#include "protocol.h"
+#include "serial.h"
+#include "spindle_control.h"
+#include "stepper.h"
+
+
+
+void setup() {
+ pinMode(12,OUTPUT); 
+
+}
+
+void loop() {
+  
+
+}
---
+++ grbl-0_7/spindle_control.c
@@ -0,0 +1,59 @@
+/*
+  spindle_control.c - spindle control methods
+  Part of Grbl
+
+  Copyright (c) 2009-2011 Simen Svale Skogsrud
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "spindle_control.h"
+#include "settings.h"
+#include "config.h"
+#include "stepper.h"
+#include "Arduino.h"
+
+#include <avr/io.h>
+
+static int current_direction;
+
+void spindle_init()
+{
+  //SPINDLE_ENABLE_DDR |= (1<<SPINDLE_ENABLE_BIT);
+  //SPINDLE_DIRECTION_DDR |= (1<<SPINDLE_DIRECTION_BIT);
+  spindle_run(0, 0);
+}
+
+void spindle_run(int direction, uint32_t rpm) 
+{
+	//printPgmString(PSTR("Spindle program "));
+  if (direction != current_direction) {
+    //st_synchronize();
+    printPgmString(PSTR("direction=!current-direction"));
+	    if(direction) {
+		     if(direction > 0) {
+	            digitalWrite(12, HIGH);
+	   	 } else {
+	           digitalWrite(12,LOW);
+		      }
+		      
+	    } 
+    current_direction = direction;
+  }
+}
+
+void spindle_stop()
+{
+  spindle_run(0, 0);
+}
